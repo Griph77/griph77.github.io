cc820fcab2a897799392a8fdd43c148db4eeea47
Griph77 <143305563+Griph77@users.noreply.github.com>
2023-08-28 16:32:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ document.getElementById("myForm").addEventListener("submit", function(event) {
     event.preventDefault(); // Evita que se recargue la página al enviar el formulario
   
     var inputData = document.getElementById("inputData").value;
-    var resultContainer = document.getElementById("result");
-  
-    resultContainer.textContent = "Datos ingresados: " + inputData;
+    var inputData2 = document.getElementById("peliculas").value;
+    var resultContainer = document.getElementById("result");    
+    var resultContainer2 = document.getElementById("result2");
+    resultContainer.textContent = "Sr(a): " + inputData;
+    resultContainer2.textContent = "Su Pelicula es: " + inputData2;
   });
